7cdce1ccb3e40238300d87d9b3f9491a786a5364
candra <zerocreateneet@gmail.com>
2024-11-02 10:48:59 +0800

    Perbaikan pada index dan script

--- script.js
+++ script.js
@@ -61,20 +61,3 @@ const activeLink = () => {
 	})
 }
 window.addEventListener('scroll',activeLink);
-
-/*~~~~~~~~~~~~~~~ ScroolReveal ~~~~~~~~~~~~~~~*/
-const sr = ScrollReveal({
-	origin: 'left',
-  reset : true,
-	distance : '80px',
-	duration : 2000,
-	delay : 200
-});
-
-sr.reveal(`.home__img, .about__img, .quality_img`,{origin: 'bottom', scale: 0.5, delay:700});
-sr.reveal(`.home__txt h1, .about__txt h2, .about__txt, .fragrance_left, .left_side, .products_left img`,{origin: 'left',interval: 500});
-sr.reveal(`.about__btn, .variety__txt p, .variety__txt img, .products_left button, .product_item`,{origin: 'bottom',interval: 700});
-sr.reveal(`.variety__txt h3, .fragrance_top, .products_top`,{origin: 'top',interval: 700});
-sr.reveal(`.fragrance_right, .right_side, .products_left`,{origin: 'right',interval: 500});
-
-
